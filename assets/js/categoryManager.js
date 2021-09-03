84444a05057822036d0041c3ab34f380ce95b57f
thomas-lecouturier <thomas.lecouturier@gmail.com>
2021-09-03 17:09:58 +0200

    Envoi d'une requete et récupération de la réponse avec fetch

--- assets/js/categoryManager.js
+++ assets/js/categoryManager.js
@@ -7,5 +7,29 @@ let categoryManager = {
      */
     loadCategories: function() {
         console.log('loadCategories');
+
+        // On prépare la configuration de la requête HTTP
+        let config = {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache'
+    };
+    
+        // On déclenche la requête HTTP (via le moteur sous-jacent Ajax)
+        fetch('https://benoclock.github.io/S07-todolist/categories.json', config)
+            // Ensuite, lorsqu'on reçoit la réponse au format JSON
+            .then(function(response) {
+                // console.log(response); // réponse entière (headers + contenu)
+                // On convertit cette réponse en un objet JS et on le retourne
+                return response.json();
+            })
+            // Ce résultat au format JS est récupéré en argument ici-même
+            .then(function(data) {
+                // On dispose désormais d'un tableau JS exploitable dans la variable data
+                // console.log(data); // contenu de la réponse sous forme d' objet JS
+
+                // TODO créer les options dans le DOM
+            }
+        );
     }
 }
